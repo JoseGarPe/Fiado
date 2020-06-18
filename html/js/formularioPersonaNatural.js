caa8e94f08168de2bdd74306063a492b8f0938a7
Vladimir Quijano <v-10000@hotmail.com>
2020-06-18 15:39:24 -0600

    arreglo de movimiento js
    formulario de persona normal con efecto del scroll

--- html/js/formularioPersonaNatural.js
+++ html/js/formularioPersonaNatural.js
@@ -5,28 +5,34 @@ var opacity;
 
 $(".next").click(function(){
 
-current_fs = $(this).parent();
-next_fs = $(this).parent().next();
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
 
-//Add Class Active
-$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	//Add Class Active
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-//show the next fieldset
-next_fs.show();
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
+	//show the next fieldset
+	next_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+	step: function(now) {
+	// for making fielset appear animation
+	opacity = 1 - now;
 
-current_fs.css({
-'display': 'none',
-'position': 'relative'
-});
-next_fs.css({'opacity': opacity});
-},
-duration: 600
-});
+	current_fs.css({
+		'display': 'none',
+		'position': 'relative'
+	});
+
+	next_fs.css({
+		'opacity': opacity
+
+	});
+	},
+		duration: 600
+	});
+
+	$('html, body').animate({scrollTop:0}, 'slow');
 });
 
 $(".previous").click(function(){
@@ -54,6 +60,7 @@ previous_fs.css({'opacity': opacity});
 },
 duration: 600
 });
+$('html, body').animate({scrollTop:0}, 'slow');
 });
 
 $('.radio-group .radio').click(function(){
