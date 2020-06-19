f6ca5c9f69a57ed270a59c08b718da0d5854e80a
Vladimir Quijano <v-10000@hotmail.com>
2020-06-19 12:23:12 -0600

    formulario de subir documentos persona natural
    diseño funcional para subir los archivos en el formulario de persona normal

--- html/js/formularioPersonaNatural.js
+++ html/js/formularioPersonaNatural.js
@@ -73,3 +73,75 @@ return false;
 })
 
 });
+
+const duiArchivo = document.getElementById("dui-file");
+const botonDui = document.getElementById("dui-button");
+const textoDui = document.getElementById("dui-text");
+
+botonDui.addEventListener("click", function() {
+  duiArchivo.click();
+});
+
+duiArchivo.addEventListener("change", function() {
+  if (duiArchivo.value) {
+    textoDui.innerHTML = duiArchivo.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    textoDui.innerHTML = "No has ingresado tu Dui.";
+  }
+});
+
+const archivoNit = document.getElementById("nit-file");
+const botonNit = document.getElementById("nit-button");
+const textoNit = document.getElementById("nit-text");
+
+botonNit.addEventListener("click", function() {
+  archivoNit.click();
+});
+
+archivoNit.addEventListener("change", function() {
+  if (archivoNit.value) {
+    textoNit.innerHTML = archivoNit.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    textoNit.innerHTML = "No has ingresado tu Nit.";
+  }
+});
+
+const archivotIVA = document.getElementById("tIVA-file");
+const botontIVA = document.getElementById("tIVA-button");
+const textotIVA = document.getElementById("tIVA-text");
+
+botontIVA.addEventListener("click", function() {
+  archivotIVA.click();
+});
+
+archivotIVA.addEventListener("change", function() {
+  if (archivotIVA.value) {
+    textotIVA.innerHTML = archivotIVA.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    textotIVA.innerHTML = "No has ingresado tu tarjeta de IVA.";
+  }
+});
+
+const archivoluzAgua = document.getElementById("luzAgua-file");
+const botonluzAgua = document.getElementById("luzAgua-button");
+const textoluzAgua = document.getElementById("luzAgua-text");
+
+botonluzAgua.addEventListener("click", function() {
+  archivoluzAgua.click();
+});
+
+archivoluzAgua.addEventListener("change", function() {
+  if (archivoluzAgua.value) {
+    textoluzAgua.innerHTML = archivoluzAgua.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    textoluzAgua.innerHTML = "No has ingresado tu recibo de agua y luz.";
+  }
+});
